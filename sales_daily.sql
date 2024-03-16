4f3ffee591c4451ecbd22950e39b24f2b5c66871
Tabatha Wiggers <50644696+tatchiwiggers@users.noreply.github.com>
2024-03-16 19:10:35 -0300

    small fixes

--- sales_daily.sql
+++ sales_daily.sql
@@ -1,6 +1,6 @@
 SELECT
     date_date
-    , ROUND (SUM(turnonver), 2) AS turnover
+    , ROUND (SUM(turnover), 2) AS turnover
     , ROUND (SUM(turnover_before_promo) -SUM(turnover), 2) AS discount
     FROM `taxifare-379314.batch1552recap.gwz_sales`
     GROUP BY date_date
