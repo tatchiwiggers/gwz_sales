fdb558200e744123d57dc658aab6a82cb939c413
Tabatha Wiggers <50644696+tatchiwiggers@users.noreply.github.com>
2024-03-16 19:07:16 -0300

    first commit

---
+++ sales_daily.sql
@@ -0,0 +1,7 @@
+SELECT
+    date_date
+    , ROUND (SUM(turnonver), 2) AS turnover
+    , ROUND (SUM(turnover_before_promo) -SUM(turnover), 2) AS discount
+    FROM `taxifare-379314.batch1552recap.gwz_sales`
+    GROUP BY date_date
+    ORDER BY date_date
