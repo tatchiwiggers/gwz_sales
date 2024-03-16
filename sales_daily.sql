4fac32e3d982d85dd0c60e824d9826f10568e16c
Tabatha Wiggers <50644696+tatchiwiggers@users.noreply.github.com>
2024-03-16 20:07:30 -0300

    add subquery

--- sales_daily.sql
+++ sales_daily.sql
@@ -1,7 +1,14 @@
+WITH tmp AS (
 SELECT
     date_date
-    , ROUND (SUM(turnover), 2) AS turnover
-    , ROUND (SUM(turnover_before_promo) -SUM(turnover), 2) AS discount
+    , ROUND (SUM(turnover)/1000, 2) AS turnover
+    , ROUND ((SUM(turnover_before_promo) - SUM(turnover))/1000, 2) AS discount
+    , ROUND ((SUM(turnover) - SUM(purchase_cost))/1000 , 2) AS margin
     FROM `taxifare-379314.batch1552recap.gwz_sales`
     GROUP BY date_date
     ORDER BY date_date
+)
+
+SELECT date_date
+, SUM(turnover) OVER (ORDER BY date_date) AS cumulative_turnover
+FROM tmp
